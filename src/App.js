e92b918a8d063b8db32c731b08e5114ff067d952
Peter Osoo <posoo617@gmail.com>
2023-02-21 18:40:54 +0300

    Hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css"
+import { Hello } from "./components/Hello"
 
 const App = () => {
 	return (
 		<div className="App">
 			<h2>New App</h2>
+			<Hello />
 		</div>
 	)
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export const Hello = () => {
+	const name = " Jaka"
+	return <div>Hello {name} </div>
+}
