2e6adb37138954394caaf280b05f9667baf2e910
Peter Osoo <posoo617@gmail.com>
2023-02-21 18:43:26 +0300

    conditions

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,5 +2,12 @@ import React from "react"
 
 export const Hello = () => {
 	const name = " Jaka"
-	return <div>Hello {name} </div>
+	let isLoggedIn = false
+
+	return (
+		<div>
+			<h3>Hello {name}</h3>
+			<h4> User is logged in {isLoggedIn ? "yes" : "no"} </h4>
+		</div>
+	)
 }
