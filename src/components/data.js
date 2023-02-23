6f22af281c54c794c21f577c55a60815c244ac90
Peter Osoo <posoo617@gmail.com>
2023-02-23 09:23:35 +0300

    one movie

---
+++ src/components/data.js
@@ -0,0 +1,8 @@
+const movie1 = {
+	Title: "Sistas",
+	Year: "2020–",
+	imdbID: "tt12043918",
+	Type: "series",
+	Poster:
+		"https://m.media-amazon.com/images/M/MV5BYjRhM2YwYjktMWRjNy00ZTczLWJmN2UtMTc3MzViMGVkMjRjXkEyXkFqcGdeQXVyMTkxMzg5MA@@._V1_SX300.jpg",
+}
